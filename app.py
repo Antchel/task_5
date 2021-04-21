68f358fc6b3622bd960423bace3e40a3f4baa969
Anton Golovenko <golan349@gmail.com>
2021-04-21 22:51:03 +0500

    Stable version

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 from flask import Flask, request, jsonify
 import sqlite3 as lite
+from newsapi import NewsApiClient
+from config import newsApi_token
+
+user_cred = 0
 
 app = Flask(__name__)
 
@@ -7,29 +11,148 @@ con = lite.connect('test.db', check_same_thread=False)
 cur = con.cursor()
 
 
-@app.route('/subscriptions/categories/<operation_id>', methods=['GET', 'POST'])
-def categories(operation_id):
-    return f'Category operation number: {operation_id}'
+def add_user(user_id):
+    global user_cred
+    user_cred = user_id
+    cur.execute('CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY,'
+                ' f_name varchar(50), l_name varchar(50));')
+    cur.execute('CREATE TABLE IF NOT EXISTS categories (category_id INTEGER PRIMARY KEY AUTOINCREMENT,'
+                'cat_name varchar(100), user_id INTEGER)')
+    cur.execute('CREATE TABLE IF NOT EXISTS keywords (keyword_id integer primary key AUTOINCREMENT,'
+                'word_name varchar(100), user_id INTEGER)')
+    user_data = cur.execute(f"SELECT * FROM users WHERE user_id = {user_id}").fetchone()
+    if user_data is None:
+        cur.execute(f"INSERT INTO users (user_id) VALUES "
+                    f" ({user_id})")
+        con.commit()
+        return "User added"
+    return f"Hello {user_id}"
 
 
-@app.route('/subscriptions/keywords/<operation_id>', methods=['GET', 'POST'])
-def keywords(operation_id):
+def add_category(category):
+    cat_data = cur.execute(f"SELECT * FROM categories WHERE cat_name = '{category}' "
+                           f"AND user_id = {user_cred}").fetchone()
+    print(cat_data)
+    if cat_data is None:
+        cur.execute(f"INSERT INTO categories (cat_name, user_id) VALUES "
+                    f" ('{category}',"
+                    f" {user_cred})")
+        con.commit()
+        return "Category was added"
+    else:
+        return "This category is already exists"
+
+
+def add_keyword(message):
+    key_data = cur.execute(f"SELECT * FROM keywords WHERE word_name = '{message}' "
+                           f"AND user_id = {user_cred}").fetchone()
+    if key_data is None:
+        cur.execute(f"INSERT INTO keywords (word_name, user_id) VALUES "
+                    f" ('{message}',"
+                    f" {user_cred})")
+        con.commit()
+        return "Keyword was added successfully"
+    else:
+        return "This keyword is already exists"
+
+
+def show_categories():
+    user_cats = cur.execute(f"SELECT cat_name FROM categories WHERE user_id = {user_cred}").fetchall()
+    if user_cats is None:
+        return "You haven't any categories"
+    else:
+        return f"List of your chosen categories {user_cats}"
+
+
+def show_keywords():
+    user_keyw = cur.execute(f"SELECT word_name FROM keywords WHERE user_id = {user_cred}").fetchall()
+    if user_keyw is None:
+        return "You haven't any keywords"
+    else:
+        return f"List of your chosen categories : {user_keyw}"
+
+
+def remove_category(cat_name):
+    cur.execute(f"DELETE FROM categories WHERE cat_name = '{cat_name}'")
+    con.commit()
+
+
+def remove_keyword(word_name):
+    cur.execute(f"DELETE FROM keywords WHERE word_name = '{word_name}'")
+    con.commit()
+
+
+@app.route('/subscriptions/categories/<category>', methods=['GET', 'PUT', 'DELETE'])
+def categories(category):
     if request.method == 'GET':
-        user = {"name": "nick", 'email': 'example.com', 'tel': ['+790853535', '+783294892384']}
-        return jsonify(user)
-    return f'Category operation number: {operation_id}'
+        cats = show_categories()
+        return jsonify(f'{cats}')
+    if request.method == 'PUT':
+        add_category(category)
+        return jsonify(f'Category was added')
+    if request.method == 'DELETE':
+        remove_category(category)
+        return f'Category {category} was DELETED'
 
 
-@app.route('/users/', methods=['GET', 'POST'])
-def users():
-    user = {"name": "nick", 'email': 'example.com', 'tel': ['+790853535', '+783294892384']}
-    return jsonify(user)
+@app.route('/subscriptions/keywords/<keyword>', methods=['GET', 'PUT', 'DELETE'])
+def keywords(keyword):
+    if request.method == 'GET':
+        keywords = show_keywords()
+        return jsonify(keywords)
+    if request.method == 'PUT':
+        add_keyword(keyword)
+        return jsonify(f'keyword was added')
+    if request.method == 'DELETE':
+        remove_keyword(keyword)
+        return f'keyword {keyword} was DELETED'
+
+
+@app.route('/users/<user_id>', methods=['GET'])
+def users(user_id):
+    if request.method == 'GET':
+        ret = add_user(user_id)
+        return jsonify(ret)
+    return jsonify("Error mzfk")
 
 
 @app.route('/news/', methods=['GET'])
 def get_news():
-    news = {"News": "All is good"}
-    return jsonify(news)
+    newsapi = NewsApiClient(api_key=newsApi_token)
+    if request.method == 'GET':
+        user_cats = cur.execute(
+            f"SELECT cat_name FROM categories WHERE user_id = {user_cred}").fetchall()
+        user_keyw = cur.execute(
+            f"SELECT word_name FROM keywords WHERE user_id = {user_cred}").fetchall()
+
+        category_list = [item for t in user_cats for item in t]
+        keyword_list = [item for t in user_keyw for item in t]
+        links = []
+        titles = []
+        for cat in category_list:
+            for keyword in keyword_list:
+                top_headlines = newsapi.get_top_headlines(q=keyword,
+                                                          category=cat,
+                                                          page_size=10,
+                                                          page=1)
+                print( f"News category is \"{cat}\"\n News keyword is \"{keyword}\"\n")
+
+                if top_headlines['totalResults']:
+                    if top_headlines['totalResults'] > 10:
+                        cnt = 10
+                    else:
+                        cnt = top_headlines['totalResults']
+                    for i in range(cnt):
+                        print( f"====== {i + 1} Article =========\n")
+                        print( f" Title \n {top_headlines['articles'][i]['title']}\n"
+                              f" Link {top_headlines['articles'][i]['url']}\n")
+                        links.append(top_headlines['articles'][i]['url'])
+                        titles.append(top_headlines['articles'][i]['title'])
+                else:
+                    print("Can't found any news!\n")
+
+        return jsonify(ok = 200, link = links)
+    return jsonify("ok")
 
 
 if __name__ == '__main__':
--- tg_bot.py
+++ tg_bot.py
@@ -2,6 +2,7 @@ import requests
 import telebot
 from config import telegram_token
 
+state = 0
 
 bot = telebot.TeleBot(telegram_token, parse_mode=None)
 keyboard = telebot.types.ReplyKeyboardMarkup(True)
@@ -11,9 +12,11 @@ keyboard.row('Remove category', 'Remove keyword')
 
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    a = requests.get('http://127.0.0.1:5000/users/')
+def send_start(message):
+    a = requests.get(f'http://127.0.0.1:5000/users/{message.from_user.id}')
     print(a.json())
+    bot.reply_to(message, f"Hello {message.from_user.first_name}, nice to meet you!\n Choose what you want!",
+                 reply_markup=keyboard)
 
 
 @bot.message_handler(commands=['help'])
@@ -21,15 +24,62 @@ def send_welcome(message):
     a = requests.get('http://127.0.0.1:5000/rest')
     print(a.json())
     bot.reply_to(message, f"msg = {message.text} There is a short list of possible commands\n"
+                          f"Yuo should start you discussion with bot with /start command\n"
                           f"/start - this command registry user (if he isn't registry) and opens main menu\n"
                           f"/show_news - this command show news based on chosen categories and keywords\n")
 
 
 @bot.message_handler(commands=['show_news'])
 def get_news(message):
-    a = requests.get('http://127.0.0.1:5000/news')
-    print(a.json())
-    bot.reply_to(message, a.json()['News'])
+    a = requests.get('http://127.0.0.1:5000/news/')
+    for i in range(len(a.json()['link'])):
+        bot.reply_to(message, (a.json()['link'][i]))
+
+
+@bot.message_handler(content_types=["text"])
+def main(message):
+    global state
+
+    if state == 1:
+        print(message.text)
+        requests.put(f'http://127.0.0.1:5000/subscriptions/categories/{message.text}')
+        state = 0
+    elif state == 2:
+        print(message.text)
+        requests.put(f'http://127.0.0.1:5000/subscriptions/keywords/{message.text}')
+        state = 0
+    elif state == 5:
+        requests.delete(f'http://127.0.0.1:5000/subscriptions/categories/{message.text}')
+        state = 0
+    elif state == 6:
+        requests.delete(f'http://127.0.0.1:5000/subscriptions/keywords/{message.text}')
+        state = 0
+        # FIXME: Реализовать "вечную" клавиатуру
+
+    bot.send_message(message.chat.id, 'done', reply_markup=telebot.types.ReplyKeyboardRemove())
+    if message.text == "Add news category":
+        state = 1
+        keyboard1 = telebot.types.ReplyKeyboardMarkup(True)
+        keyboard1.row('sports', 'business')
+        keyboard1.row('entertainment', 'general')
+        keyboard1.row('health', 'science')
+        keyboard1.row('technology')
+        bot.send_message(message.chat.id, "Choose the possible categories", reply_markup=keyboard1)
+    elif message.text == 'Add news keyword':
+        state = 2
+        bot.send_message(message.chat.id, "Enter new keyword name")
+    elif message.text == 'Show my categories':
+        a = requests.get(f'http://127.0.0.1:5000/subscriptions/categories/{1}')
+        bot.send_message(message.chat.id, f"{a.json()}")
+    elif message.text == 'Show my keywords':
+        a = requests.get(f'http://127.0.0.1:5000/subscriptions/keywords/{1}')
+        bot.send_message(message.chat.id, f"{a.json()}")
+    elif message.text == 'Remove category':
+        state = 5
+        bot.send_message(message.chat.id, "Enter name of category what you want to delete")
+    elif message.text == 'Remove keyword':
+        state = 6
+        bot.send_message(message.chat.id, "Enter name of keyword what you want to delete")
 
 
 bot.polling()
